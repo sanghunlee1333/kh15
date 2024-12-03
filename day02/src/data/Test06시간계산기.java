9c5ff84888279f9d4d43cf54a9f5aefd186f38ab
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-03 14:57:10 +0900

    day02 시간계산기 (피드백)

--- day02/src/data/Test06시간계산기.java
+++ day02/src/data/Test06시간계산기.java
@@ -3,6 +3,7 @@ package data;
 public class Test06시간계산기 {
 
 	public static void main(String[] args) {
+		/*
 		int time1 = 110;
 		int time2 = 165;
 		
@@ -15,11 +16,29 @@ public class Test06시간계산기 {
 		int totalMinute = minute1 + minute2;
 		
 		if(totalMinute >= 60) {
-			totalHour++;
+			totalHour = totalHoute + totalMinute/60;
 			totalMinute = totalMinute%60;
 		}
 		
-		System.out.println(totalHour +"시간" + " " + totalMinute + "분");
+		System.out.println(totalHour + "시간" + " " + totalMinute + "분");
+		*/
+		
+		//입력
+		int h1 = 1, m1 = 50;
+		int h2 = 2, m2 = 45;
+		
+		//처리 - 가장 작은 단위로 변환하여 계산
+		int t1 = h1 * 60 + m1;
+		int t2 =h2 * 60 + m2;
+		int t3 = t1 + t2;
+		
+		int h3 = t3 / 60;
+		int m3 = t3 % 60;
+		
+		//출력
+		System.out.println(h3);
+		System.out.println(m3);
+		
 	}
 
 }
