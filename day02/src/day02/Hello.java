ea887b216e2706ef940e1c8d443c6f4b0dad5f02
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-03 11:03:49 +0900

    띄어쓰기 추가

--- day02/src/day02/Hello.java
+++ day02/src/day02/Hello.java
@@ -6,6 +6,6 @@ public class Hello { //클래스 영역(집)
 		//프로그램 실행 시 나올 내용에 대한 코드
 		//출력 명령
 		System.out.println("Hello World");
-		System.out.println("Hello Java!");
+		System.out.println("Hello Java!"); 
 	}
 }
