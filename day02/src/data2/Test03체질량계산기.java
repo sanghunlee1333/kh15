6976c0a24483bd49e15ba5d2e2ba10c88a9b9a9e
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-03 18:09:52 +0900

    day02 체질량 계산기 과제 수정

--- day02/src/data2/Test03체질량계산기.java
+++ day02/src/data2/Test03체질량계산기.java
@@ -5,7 +5,7 @@ public class Test03체질량계산기 {
 	public static void main(String[] args) {
 		int weight = 80;
 		float height = 1.80f;
-	
+
 		float bmi = weight / (height * height);
 
 		System.out.println(bmi);
