f6b009304ee20345e7e1cab440a5fd8240c05919
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-03 10:53:40 +0900

    day02 생성

---
+++ day02/src/day02/Hello.java
@@ -0,0 +1,11 @@
+//클래스의 위치(집 주소) ex)신사동 - 관악구 신사동, 은평구 신사동 등
+package day02;
+
+public class Hello { //클래스 영역(집)
+	public static void main(String args[]) { //메소드 영역(방) ex) 거실, 화장실 등
+		//프로그램 실행 시 나올 내용에 대한 코드
+		//출력 명령
+		System.out.println("Hello World");
+		System.out.println("Hello Java!");
+	}
+}
