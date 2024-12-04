7353d484ee57cfe47db70c33c82ba9bd697437db
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-04 10:02:41 +0900

    day02 체질량 계산기 피드백

--- day02/src/data2/Test03체질량계산기.java
+++ day02/src/data2/Test03체질량계산기.java
@@ -3,11 +3,30 @@ package data2;
 public class Test03체질량계산기 {
 
 	public static void main(String[] args) {
+		/*
 		int weight = 80;
-		float height = 1.80f;
+		int height = 180;
+		double heightSquare = height * height * 0.0001;
 
-		float bmi = weight / (height * height);
+		double bmi = weight / heightSquare;
 
+		System.out.println(bmi);
+		*/
+		
+		//입력
+		int cm = 180;
+		int kg = 80;
+		//float cm = 180;
+		//float kg = 80;
+				
+		//처리
+		float m = cm / 100f; //100;
+		float bmi = kg / (m * m);
+		//float bmi = kg / m / m;
+		
+		//float와 double을 혼용해서 쓰면 안된다. 소수점끼리 계산이 복잡해짐. 한 가지만 쓰자.
+		
+		//출력
 		System.out.println(bmi);
 	}
 
