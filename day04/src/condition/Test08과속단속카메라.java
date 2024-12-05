fa7e6b5de46b330f5b885cbe21593860377ea17a
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-05 18:44:42 +0900

    day04 과속단속카메라 과제 업로드

--- day04/src/condition/Test08과속단속카메라.java
+++ day04/src/condition/Test08과속단속카메라.java
@@ -12,23 +12,20 @@ public class Test08과속단속카메라 {
 		int speed = sc.nextInt();
 		int limit = 110;
 		
-		int price;
 		int addition = 10000;
-		
+		int penalty = (speed - limit) / 10 * addition;
+		int price = 40000 + penalty;
+				
 		System.out.println("자동차의 속력 : " + speed + "km");
 		if (speed < 110) {
 			System.out.println("범칙금이 없습니다");
 		}
-		else if (speed >= 110) {
-			int penalty = (speed - limit) / 10 * addition;
-			price = 40000 + penalty;
-			if (price >= 100000) {
-				price = 100000;
-				System.out.println("범칙금은 " + price + "원입니다.");
-			}
-			else {
+		else if (price >= 100000) {
+			price = 100000;
 			System.out.println("범칙금은 " + price + "원입니다.");
-			}
+		}
+		else {
+			System.out.println("범칙금은 " + price + "원입니다.");
 		}
 
 	}
