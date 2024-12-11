246cf5364a3c1739ebedabb3300e388162346331
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-11 17:33:47 +0900

    백준

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -4,15 +4,16 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
-		
-		while (sc.hasNextInt()) {
-			int A = sc.nextInt();
-			int B = sc.nextInt();
-			
-			System.out.println(A + B);
-			
-
+		int a = sc.nextInt();
+		for (int k = 0; k < a; k++) {
+			for (int j = a; j > 0; j--) {
+				for (int i = a-4; i < j; i++) {
+					System.out.print(" ");
+				}
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
+
 }
