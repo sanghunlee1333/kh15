5faf244cb7b2d67f5aeb4e96df2d1eebd1373c50
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-12 15:44:55 +0900

    day09

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -4,16 +4,22 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		for (int k = 0; k < a; k++) {
-			for (int j = a; j > 0; j--) {
-				for (int i = a-4; i < j; i++) {
-					System.out.print(" ");
-				}
-				System.out.print("*");
-			}
-			System.out.println();
+		int[] a = new int[9];
+		
+		int max = 0;
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
 		}
+		for (int i = 1; i < a.length; i++) {
+			if(a[i] > a[max]) {
+				max = i;
+			}
+		}
+
+		
+		System.out.println(a[max]);
+		System.out.println(max+1);
+
 	}
 
 }
