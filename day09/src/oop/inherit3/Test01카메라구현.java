02f62118d1004312792d5859aa1bff1eff73e13d
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-13 10:01:06 +0900

    day09 카메라 과제 피드백

--- day09/src/oop/inherit3/Test01카메라구현.java
+++ day09/src/oop/inherit3/Test01카메라구현.java
@@ -8,19 +8,21 @@ public class Test01카메라구현 {
 		ActionCam a1 = new ActionCam();
 		DigitalCam d1 = new DigitalCam();
 		
-		w1.setName("웹캠");
-		w1.setPixel(1000000);
+		//w1.setName("웹캠");
+		//w1.setPixel(1000000);
 		w1.record();
 		w1.decorate();
 		
-		a1.setName("액션캠");
-		a1.setPixel(5000000);
+		//a1.setName("액션캠");
+		//a1.setPixel(5000000);
 		a1.record();
+		a1.slowMotion();
 		a1.reduce();
 		
-		d1.setName("디지털캠");
-		d1.setPixel(10000000);
+		//d1.setName("디지털캠");
+		//d1.setPixel(10000000);
 		d1.record();
+		d1.slowMotion();
 		d1.wideRecord();
 		
 	}
