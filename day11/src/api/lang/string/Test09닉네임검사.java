6a7ba5f836912478446a9146e6660df3a4cfb362
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-17 01:25:01 +0900

    day11 닉네임 검사 과제 제출

--- day11/src/api/lang/string/Test09닉네임검사.java
+++ day11/src/api/lang/string/Test09닉네임검사.java
@@ -8,36 +8,39 @@ public class Test09닉네임검사 {
 		Scanner sc = new Scanner(System.in);
 
 		String nickname = sc.next();
+		StringBuffer buffer = new StringBuffer();
+		
+		boolean nicknameLength = nickname.length() >= 2 && nickname.length() <= 10; // 2글자 이상 10글자 이하인지
+		boolean hasOperator = nickname.contains("운영자"); //운영자를 포함하고 있는지
+		boolean hasFirst = nickname.charAt(0) >= (char)'0' && nickname.charAt(0) <= '9'; //첫 글자가 0과 9 사이인지
+		
+		if (nicknameLength) { 
+			if (!hasOperator) {
+				if (!hasFirst) { 
+					for (int i = 0; i < nickname.length(); i++) { // 첫 번째 글자부터 마지막 글자까지
+						if (nickname.charAt(i) >= (char)'0' && nickname.charAt(i) <= (char)'9' // 각 글자가 0 ~ 9 사이의 숫자인지
+								|| nickname.charAt(i) >= '가' && nickname.charAt(i) <= '힣') { // 혹은 각 글자가 가 ~ 힣 사이의 문자인지
+							buffer.append(nickname.charAt(i)); // 맞으면 문자열 합성
+						} else { // 한글 또는 숫자 외에 다른 것이 있다면
+							System.out.println("닉네임 형식에 맞지 않습니다");
+							break;
+						}
+					}				
+				} else { // 첫 번째 글자가 숫자라면
+					System.out.println("닉네임 형식에 맞지 않습니다");
+				}
 
-		if (nickname.length() < 2 || nickname.length() > 10) {
+			} else { // "운영자"가 포함되어 있다면
+				System.out.println("닉네임 형식에 맞지 않습니다");
+			}
+
+		} else { // 2글자 미만 10글자 초과라면
 			System.out.println("닉네임 형식에 맞지 않습니다");
-		}
-
-		else if (nickname.contains("운영자")) {
-			System.out.println("닉네임 형식에 맞지 않습니다");
-		}
-
-		for (int i = '0'; i <= '9'; i++) {
-			if (nickname.charAt(0) == (char) i) {
-				System.out.println("닉네임 형식에 맞지 않습니다");
-				break;
-			}
-		}
-
-		for (int i = 0; i < nickname.length(); i++) {
-			if (nickname.charAt(i) < '가' && nickname.charAt(i) > '힣') {
-				System.out.println("닉네임 형식에 맞지 않습니다");
-				break;
-			}
-		}
-		for (int i = 1; i < nickname.length(); i++) {
-			if (nickname.charAt(i) < '0' || nickname.charAt(i) > '9' || nickname.charAt(i) < '가' || nickname.charAt(i) > '힣') {
-				System.out.println("닉네임 형식에 맞지 않습니다");
-				break;
-			}
-			System.out.println("닉네임 설정 완료");
 		}
 		
+		if (nickname.equals(buffer.toString())){ // 닉네임이 buffer에서 쌓은 문자와 최종적으로 같다면
+			System.out.println("닉네임 설정 완료");
+		}
 
 	}
 
