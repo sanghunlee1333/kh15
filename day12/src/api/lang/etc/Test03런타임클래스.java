3b12ac5108b154912f956c8048d8c95dbee79de3
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-17 16:42:42 +0900

    day12 Runtime 클래스

--- day12/src/api/lang/etc/Test03런타임클래스.java
+++ day12/src/api/lang/etc/Test03런타임클래스.java
@@ -1,10 +1,30 @@
 package api.lang.etc;
 
+import java.io.IOException;
+
 public class Test03런타임클래스 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- 
+	public static void main(String[] args) throws IOException {
+		// Runtime 클래스
+		// - 외부 실행환경에 명령을 보낼 수 있는 도구
+		// - 생성자가 없지만 생성 명령이 있는 경우
+
+		// Runtime rt = new Runtime(); (X)
+		Runtime rt = Runtime.getRuntime();
+
+		String os = System.getProperty("os.name");
+		if (os.toLowerCase().startsWith("window")) {
+// 			rt.exec("notepad");
+//			rt.exec("mspaint");
+//			rt.exec("calc");
+//			rt.exec("cmd /c start https://iei.or.kr"); 
+			rt.exec(new String[] { "cmd", "/c", "start", "https://iei.or.kr" });
+		}
+		else if(os.toLowerCase().startsWith("mac")) {
+			rt.exec(new String[] {"open", "-a", "TextEdit.app"});
+			rt.exec(new String[] {"open", "https://iei.or.kr"});
+		}
+		
 	}
 
 }
