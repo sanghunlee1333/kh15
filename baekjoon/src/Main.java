441835628c30f46d1a7cf5dc50cad0622cb9ab84
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-19 14:58:46 +0900

    백준

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -3,39 +3,53 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		int[] a = new int[28];
-		int[] b = new int[30];
+//		Scanner sc = new Scanner(System.in);
+//		int[] a = new int[28];
+//		int[] b = new int[30];
+//		
+//		for (int i = 0; i < b.length; i++) {
+//			b[i] = i;
+//		} 
+//		
+//		for (int i = 0; i < a.length; i++) {
+//			a[i] = sc.nextInt();
+//		}
+//		boolean[] c = new boolean[b.length];
+//		
+//		for (int j = 0; j < b.length; j++) {
+//			for (int i = 0; i < a.length; i++) {
+//				if (b[j] == a[i]) {
+//					c[j] = true;
+//				}
+//				else {
+//					c[j] = false;
+//				}
+//			}
+//				
+//		}
+//		
+//		for(int j = 0; j < b.length; j++) {
+//			if(c[j] == false) {
+//				System.out.println(j);
+//			}
+//		}
 		
-		for (int i = 0; i < b.length; i++) {
-			b[i] = i;
-		} 
 		
-		for (int i = 0; i < a.length; i++) {
-			a[i] = sc.nextInt();
-		}
-		boolean[] c = new boolean[b.length];
-		
-		for (int j = 0; j < b.length; j++) {
-			for (int i = 0; i < a.length; i++) {
-				if (b[j] == a[i]) {
-					c[j] = true;
-				}
-				else {
-					c[j] = false;
-				}
+			Scanner sc = new Scanner(System.in);
+			
+			int caseNum = sc.nextInt();
+			String[] a = new String[caseNum];
+						
+			for (int i = 0; i < caseNum; i++) {
+				a[i] = sc.next();
 			}
-				
-		}
-		
-		for(int j = 0; j < b.length; j++) {
-			if(c[j] == false) {
-				System.out.println(j);
+			for (int i = 0; i < caseNum; i++) {
+				System.out.print(a[i].charAt(0));
+				System.out.print(a[i].charAt(a[i].length()-1));
+				System.out.println();
 			}
+			
+	        
 		}
-		
-	
-
-	}
 
 }
