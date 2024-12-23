0d8e337a522fdca57e8a4e369ff8471fad2947da
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-23 09:42:48 +0900

    day16

---
+++ day16/src/api/collection/Test11리스트의종류.java
@@ -0,0 +1,19 @@
+package api.collection;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+public class Test11리스트의종류 {
+
+	public static void main(String[] args) {
+		//선형구조
+		//- 저장소의 시작과 끝이 명확한 형태
+		List<String> a = new ArrayList<>();
+		List<String> b = new LinkedList<>();
+		List<String> c = new Vector<>();
+		
+	}
+
+}
