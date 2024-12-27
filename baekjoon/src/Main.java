8a159fc526eca84a749a2c059ecb39130c4bec00
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-27 17:29:56 +0900

    백준

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -1,15 +1,40 @@
-import java.math.BigDecimal;
-import java.math.MathContext;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 
-		System.out.println("         ,r'\"7");
-		System.out.println("r`-_   ,'  ,/");
-		System.out.println(" \\. \". L_r'");
-		System.out.println("   `~\\/");
-		System.out.println("      |");
-		System.out.println("      |");
+		Scanner sc = new Scanner(System.in);
+		
+		int king = sc.nextInt();
+		int queen = sc.nextInt();
+		int rook = sc.nextInt();
+		int bishop = sc.nextInt();
+		int fone = sc.nextInt();
+		
+		if(!(king == 1)) {
+			System.out.print(king - (king - 1) + " ");
+		}
+		else System.out.print(king);
+		
+		if(!(queen == 1)) {
+			System.out.print(queen - (queen - 1) + " ");
+		}
+		else System.out.print(queen);
+		
+		if(!(rook == 2)) {
+			System.out.print(rook - (rook - 2) + " ");
+		}
+		else System.out.print(rook);
+		
+		if(!(queen == 1)) {
+			System.out.print(queen - (queen - 1) + " ");
+		}
+		else System.out.print(queen);
+		
+		if(!(queen == 1)) {
+			System.out.print(queen - (queen - 1) + " ");
+		}
+		else System.out.print(queen);
 
 	}
 
