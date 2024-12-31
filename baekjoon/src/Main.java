ef80721dcee7d6e74f46940bf5b22988bea2c07e
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-31 18:10:20 +0900

    백준

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -36,25 +36,48 @@ public class Main {
 //		}
 //		else System.out.print(queen);
 		
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int[][] numbers = new int[n][m];
-		
-		for(int i = 0; i < 2; i++) {
-			for (int k = 0; k < numbers.length; k++) {
-				for(int j = 0; j < numbers[m].length; j++) {
-					numbers[k][j] = sc.nextInt();
-				}
-			}
-		}
-		
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		int m = sc.nextInt();
+//		int[][] numbers = new int[n][m];
+//		
+//		for(int i = 0; i < 2; i++) {
+//			for (int k = 0; k < numbers.length; k++) {
+//				for(int j = 0; j < numbers[m].length; j++) {
+//					numbers[k][j] = sc.nextInt();
+//				}
+//			}
+//		}
+//		
 //		for (int k = 0; k < numbers.length; k++) {
 //			for(int j = 0; j < numbers[m].length; j++) {
 //				numbers[0][0] + numbers[3][1]
+////			}
+////		}
+//		
+//			String control = "wasdwasdas";
+//			int[] numlog = new int[] {};
+//			for (int i = 0; i < control.length(); i++) {
+//				if(control.charAt(i) == 'w') n++;
+//				else if(control.charAt(i) == 's') n--;
+//				else if(control.charAt(i) == 'd') n += 10;
+//				//else n -= 10;
 //			}
-//		}
+//		
 		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i = n; i >= 1; i--) {
+			System.out.println();
+			for (int k = 1; k < n; k++) {
+				System.out.print(" ");	
+										
+			}
+			System.out.print("*");
+		}
+	        
+	        
 	
 
 	}
