65ee06f65e12711ddefe1ad7c16c329b48062839
sanghunlee1333 <sanghunlee1333@gmail.com>
2024-12-31 18:15:29 +0900

    연결 도구 생성 (모듈화)

---
+++ jdbc01/src/jdbc/util/JdbcFactory.java
@@ -0,0 +1,21 @@
+package jdbc.util;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class JdbcFactory {
+	
+	//모듈화
+	//사용자가 쉽게 이용 가능하도록 JdbcTemplate을 생성하는 메소드
+	public static JdbcTemplate createTemplate() { //반환형이 JdbcTemplate
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:xe"); 
+		dataSource.setUsername("kh15");
+		dataSource.setPassword("kh15");
+
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		return jdbcTemplate;
+	}
+	
+}
