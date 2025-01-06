b28509e4f9dca03e6f4b364caf97b0d3aef55b0c
sanghunlee1333 <sanghunlee1333@gmail.com>
2025-01-06 18:02:10 +0900

    컨트롤러 구현

---
+++ spring04jdbc/src/main/java/com/kh/spring04/controller/CountryController.java
@@ -0,0 +1,31 @@
+package com.kh.spring04.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/country") //이 컨트롤러의 모든 매핑에 공용주소 부여
+public class CountryController {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@RequestMapping("/insert")
+	public String insert(
+		@RequestParam String countryName,
+		@RequestParam String countryCapital,
+		@RequestParam long countryPopulation
+	  ){
+		String sql = "insert into country("
+				+ "country_no, country_name, country_capital, country_population"
+				+ ") values(country_seq.nextval, ?, ?, ?)";
+		Object[] data = {countryName, countryCapital, countryPopulation};
+		jdbcTemplate.update(sql, data);
+		
+		return "나라 정보 등록 완료";
+	}
+
+}
--- spring04jdbc/src/main/java/com/kh/spring04/controller/PokemonController.java
+++ spring04jdbc/src/main/java/com/kh/spring04/controller/PokemonController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/pokemon")
 public class PokemonController {
 	
 	//여기서 어떻게 JdbTemplate를 쓴다는 건가?
