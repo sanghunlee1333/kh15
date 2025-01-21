5b35230ee34147d27c7a8798d2a4b0ee6280ad2f
sanghunlee1333 <sanghunlee1333@gmail.com>
2025-01-21 16:37:46 +0900

    연결테이블.sql

---
+++ database/연결테이블.sql
@@ -0,0 +1,19 @@
+create table pokemon_image (
+	pokemon_no references pokemon(pokemon_no) on delete cascade,
+	attachment_no references attachment(attachment_no) on delete cascade
+);
+
+create table country_flag (
+	country_no references country(country_no) on delete cascade,
+	attachment_no references attachment(attachment_no) on delete cascade
+);
+
+create table game_user_profile (
+	game_user_no references game_user(game_user_no) on delete cascade,
+	attachment_no references attachment(attachment_no) on delete cascade
+);
+
+
+select * from pokemon_image;
+select * from country_flag;
+select * from game_user_profile;
