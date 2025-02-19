8b7cbb85d266a3c243d4c5c8bdbcd8a7daae07fe
sanghunlee1333 <sanghunlee1333@gmail.com>
2025-02-19 18:17:45 +0900

    Update 테이블_좋아요.sql

--- database/테이블_좋아요.sql
+++ database/테이블_좋아요.sql
@@ -4,3 +4,14 @@ board_no references board(board_no) on delete cascade,
 primary key(member_id, board_no)
 --unique(member_id, board_no)
 );
+
+
+--포켓몬 테이블에 좋아요 항목 추가
+alter table pokemon add(pokemon_like number) 
+
+create table pokemon_like (
+member_id references member(member_id) on delete cascade,
+pokemon_no references pokemon(pokemon_no) on delete cascade,
+primary key(member_id, pokemon_no)
+--unique(member_id, pokemon_no)
+);
