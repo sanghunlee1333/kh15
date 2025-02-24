73d7814c1b82eec3914da712b3bc58d03df98225
sanghunlee1333 <sanghunlee1333@gmail.com>
2025-02-24 09:42:06 +0900

    11번 이메일 프로젝트

---
+++ spring11email/src/main/java/com/kh/spring11/Spring11emailApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring11emailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring11emailApplication.class, args);
+	}
+
+}
