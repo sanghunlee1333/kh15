37c586d562682d2bef088a30839a558e9d74c45e
sanghunlee1333 <sanghunlee1333@gmail.com>
2025-04-03 13:42:18 +0900

    프로그래머스 풀이

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -1,163 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-//		Scanner sc = new Scanner(System.in);
-//		
-//		int king = sc.nextInt();
-//		int queen = sc.nextInt();
-//		int rook = sc.nextInt();
-//		int bishop = sc.nextInt();
-//		int fone = sc.nextInt();
-//		
-//		if(!(king == 1)) {
-//			System.out.print(king - (king - 1) + " ");
-//		}
-//		else System.out.print(king);
-//		
-//		if(!(queen == 1)) {
-//			System.out.print(queen - (queen - 1) + " ");
-//		}
-//		else System.out.print(queen);
-//		
-//		if(!(rook == 2)) {
-//			System.out.print(rook - (rook - 2) + " ");
-//		}
-//		else System.out.print(rook);
-//		
-//		if(!(queen == 1)) {
-//			System.out.print(queen - (queen - 1) + " ");
-//		}
-//		else System.out.print(queen);
-//		
-//		if(!(queen == 1)) {
-//			System.out.print(queen - (queen - 1) + " ");
-//		}
-//		else System.out.print(queen);
-
-//		Scanner sc = new Scanner(System.in);
-//		int n = sc.nextInt();
-//		int m = sc.nextInt();
-//		int[][] numbers = new int[n][m];
-//		
-//		for(int i = 0; i < 2; i++) {
-//			for (int k = 0; k < numbers.length; k++) {
-//				for(int j = 0; j < numbers[m].length; j++) {
-//					numbers[k][j] = sc.nextInt();
-//				}
-//			}
-//		}
-//		
-//		for (int k = 0; k < numbers.length; k++) {
-//			for(int j = 0; j < numbers[m].length; j++) {
-//				numbers[0][0] + numbers[3][1]
-////			}
-////		}
-//		
-//			String control = "wasdwasdas";
-//			int[] numlog = new int[] {};
-//			for (int i = 0; i < control.length(); i++) {
-//				if(control.charAt(i) == 'w') n++;
-//				else if(control.charAt(i) == 's') n--;
-//				else if(control.charAt(i) == 'd') n += 10;
-//				//else n -= 10;
-//			}
-//		
-
-//		Scanner sc = new Scanner(System.in);
-//		int n = sc.nextInt();
-//		
-//		for (int i = n; i > 0; i--) {
-//			for (int k = 0; k < i; k++) {
-//				if(k < i) {
-//					System.out.print(" ");
-//				}
-//				else {
-//					System.out.print("*");
-//				}
-//										
-//			}
-//			System.out.println();
-//		}
-
-//		Scanner sc = new Scanner(System.in);
-//		
-//		//int[] num = new int[10];
-//		int[] num = new int[] {42,84,252,420,840, 126,42,84,420,126};
-//		int[] rest = new int[10];
-//		for (int i = 0; i < num.length; i++) {
-//			//num[i] = sc.nextInt();
-//			rest[i] = num[i] % 42;
-//		}
-//		int answer = 10;
-//		int target = 0;
-//		for(int k = 0; k < rest.length; k++) {
-//			if(rest[k] == target) {
-//				answer--;
-//			}
-//		}
-//		System.out.println(answer);
-
-//		Scanner sc = new Scanner(System.in);
-//		
-//		String answer = "";
-//		
-//		while(true) {
-//			
-//			int num1 = sc.nextInt();
-//			int num2 = sc.nextInt();
-//			boolean factor = num2 % num1 == 0;
-//			boolean multiple = num1 % num2 == 0;
-//			
-//			if(num1 == 0 & num2 == 0) {
-//				break;
-//			}
-//			else
-//			if(factor) {
-//				answer = "factor";
-//				System.out.println(answer);
-//			}
-//			else if(multiple) {
-//				answer = "multiple";
-//				System.out.println(answer);
-//			}
-//			else if (!(multiple || factor)) {
-//				answer = "neither";
-//				System.out.println(answer);
-//			}
-//		}
-		
-		String regex = "^[a-z][a-z0-9\\_]{6,23}[a-z0-9]@(?=.*?[a-z])(?=.*?[0-9])[a-z0-9]{4,15}\\.(co\\.kr|net|com|org)$";
-		String test = "abcd5aaada__a@abc012.org";
-		
-//		String regex = "^[ㄱ-ㅎㅏ-ㅣ가-힣0-9]{1,10}$";
-//		String test = "ㅡㅡ";
-		
-		
-		System.out.println(test.matches(regex));
-				
-	   
-		
-
-//		int[] sides = new int[] {199, 72, 222};
-//		int answer = 0;
-//        
-//        int max = 0;
-//        int rest = 0;
-//        for(int i = 0; i < 3; i++){
-//            if(sides[i] > max){
-//                max = sides[i];
-//            }
-//            else rest += sides[i];
-//        }
-//        System.out.println(rest);
-//        
-//        if(max < rest){
-//            answer = 1;
-//        }
-//        else answer = 2;
-
+		int n = 1234;
+		int num1 = 1;
+		int num2 = 3;
+		int[] numbers = {1, 2, 3, 4, 5}; 
+		int[] answer = {};
+	    int size = num2 - num1 + 1;
+	    answer = new int[size];
+	    
+	    
+	    for (int i = 0; i < size; num1++) {
+	       answer[i] = numbers[num1];
+	    }
+	    for(int i = 0; i < answer.length; i++) {
+	    	System.out.println(answer[0]);
+	    }
 	}
 
 }
